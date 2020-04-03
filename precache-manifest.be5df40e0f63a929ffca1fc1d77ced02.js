970ca3f5fb3de9dfb8fb119ce899b0a39b2a5cf4
Michal Bryk <galeriakoloru90210@gmail.com>
2020-04-03 22:55:54 +0200

    Updates

---
+++ precache-manifest.be5df40e0f63a929ffca1fc1d77ced02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e08956d42e66aeef59c85eecf8b4161",
+    "url": "/To-Do-App/index.html"
+  },
+  {
+    "revision": "673cfa4fe8210282b939",
+    "url": "/To-Do-App/static/css/main.4cf0cb37.chunk.css"
+  },
+  {
+    "revision": "daa06daca6c25ed7dfb7",
+    "url": "/To-Do-App/static/js/2.f7b793fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-App/static/js/2.f7b793fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "673cfa4fe8210282b939",
+    "url": "/To-Do-App/static/js/main.cddd3160.chunk.js"
+  },
+  {
+    "revision": "ee4899fe6d9fdfbe1f0d",
+    "url": "/To-Do-App/static/js/runtime-main.41ed12b5.js"
+  }
+]);
